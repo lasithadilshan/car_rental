ca092fb55af1b06cee8a9b3d930e82851598dc81
Lasitha Thilakarathna <37534587+lasithadilshan@users.noreply.github.com>
2021-05-08 12:38:44 +0530

    Second Commit added github pages

--- lib/main.dart
+++ lib/main.dart
@@ -10,7 +10,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Flutter Car Rental UI',
       theme: ThemeData(
         primarySwatch: Colors.blue,
         visualDensity: VisualDensity.adaptivePlatformDensity,
